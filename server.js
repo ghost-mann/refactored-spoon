9269d54089343b414cecec815858a32de7895374
ghost-mann <iamaob8@protonmail.com>
2025-03-21 09:59:25 +0300

    feat: setting up express server in server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express();
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, { 'Content-Type': 'text/html'  });
-
-    res.write('<h1>Hello, Node.js HTTP Server</h1>')
-    res.end();
+app.get('/', (req,res) => {
+    res.send('<h1>Hello, Express.js Server</h1>');
 })
 
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Node.js HTTP server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`)
 })
